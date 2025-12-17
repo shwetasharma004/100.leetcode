6a08f4c909c011c1e225dfb9c5865361f7b9b94c
cnshwetasharma <devshwetasharma07@gmail.com>
2025-12-18 01:05:20 +0530

    Time: 4 ms (32.82%), Space: 8.4 MB (91.86%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,18 +1,20 @@
+//Greedy approach ...
 class Solution {
 public:
     bool isPalindrome(int x) {
-        int temp = x; 
-        long rev =0; 
-        while(temp!=0){
-            rev = (rev * 10)+ temp % 10;
-            temp/=10;
-            
+
+        int temp = x;
+        long int  isPalindrome = 0;    
+        while(temp != 0){          
+            int rem =  temp % 10;
+            isPalindrome = (isPalindrome * 10) + rem;
+            temp /= 10;
         }
-        if(rev == x && rev >= 0){
+        
+        if(isPalindrome == x && isPalindrome >= 0 ){
             return true;
         }
         return false;
     }
-
-
 };
+
