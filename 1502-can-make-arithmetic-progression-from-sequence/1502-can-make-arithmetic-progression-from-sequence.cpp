f4809deb2932db86b92dd5c07a2dce9b6e783ae3
Shweta Sharma <devshwetasharma07@gmail.com>
2026-02-03 01:02:26 +0530

    Time: 0 ms (100%), Space: 12.8 MB (44.72%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
@@ -1,22 +1,11 @@
 class Solution {
 public:
     bool canMakeArithmeticProgression(vector<int>& arr) {
-         if (arr.size() <= 2) {
-            return true;
-        }
-        
-        
         std::sort(arr.begin(), arr.end());
-        
-        
-        int diff = arr[1] - arr[0];
-        
-        for (size_t i = 2; i < arr.size(); i++) {
-            if (arr[i] - arr[i - 1] != diff) {
-                return false;
-            }
-        }
-        
-        return true;
+        int gap = arr[1] - arr[0];
+        for(int i=2; i<arr.size(); i++){
+          if(arr[i]-arr[i-1]!=gap) return false;
+    }
+    return true;
     }
 };
