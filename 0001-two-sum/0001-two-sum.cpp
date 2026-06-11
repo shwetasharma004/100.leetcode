4b711cb58334e29a22c29f56d29ce228da4e58bc
Shweta Sharma <devshwetasharma07@gmail.com>
2026-06-11 22:57:24 +0530

    Time: 44 ms (16.4%), Space: 14.1 MB (81.6%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,22 +1,13 @@
 class Solution {
 public:
-    int maxArea(vector<int>& height) {
-        int n = height.size();
-
-        int maxWater = 0;
-        int low = 0;
-        int high = n-1;
-
-        while(low < high){
-            int width = high-low;
-            int minHeight = min(height[low], height[high]);
-            maxWater = max(maxWater , width*minHeight);
-
-            if(height[low] < height[high])
-                low++;
-                else
-                high--;
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
+            }
         }
-        return maxWater;
+        return {};
     }
 };
